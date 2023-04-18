1f6793c7c33bb17ff6c00f50e4cb77332ee837e9
Ricardo Severo <ricardo.severo@quantosobra.com.br>
2023-04-17 22:59:54 -0300

    polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar -r main 2>&1 | tee -a /tmp/polybar1.log & disown
+if [[ $(xrandr --query | grep " connected" | grep HDMI-1 -o) == 'HDMI-1' ]]; then
+    polybar -r secondary 2>&1 | tee -a /tmp/polybar2.log & disown
+fi
+
+echo "Bars launched..."
