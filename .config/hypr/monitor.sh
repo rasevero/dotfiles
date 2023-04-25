878837fe3a4fbb56b98fc40866d91d3cb74b7b6d
Ricardo Severo <ricardo.severo@quantosobra.com.br>
2023-04-25 13:38:33 -0300

    hyprland

---
+++ .config/hypr/monitor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wlr-randr --output HDMI-A-1  &> /dev/null
+if [[ $? == 0 ]]; then
+    wlr-randr --output eDP-1 off
+    wlr-randr --output HDMI-A-1 --mode 2560x1440 --scale 1
+    wlr-randr --output eDP-1 on --pos 3926,0
+fi
