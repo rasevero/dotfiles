efc10fb59b744d373d60d14b62ebf7be2d3bd1e9
Ricardo Severo <ricardo.severo@quantosobra.com.br>
2023-05-16 09:45:36 -0300

    Dunst no hyprland e outros ajustes

--- .config/hypr/monitor.sh
+++ .config/hypr/monitor.sh
@@ -2,7 +2,7 @@
 
 wlr-randr --output HDMI-A-1  &> /dev/null
 if [[ $? == 0 ]]; then
-    wlr-randr --output eDP-1 off
-    wlr-randr --output HDMI-A-1 --mode 2560x1440 --scale 1
-    wlr-randr --output eDP-1 on --pos 3926,0
+    wlr-randr --output eDP-1 --off
+    #wlr-randr --output HDMI-A-1 --mode 2560x1440 --scale 1
+    #wlr-randr --output eDP-1 --on --pos 3926,0
 fi
