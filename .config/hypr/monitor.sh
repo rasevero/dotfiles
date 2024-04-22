525e0e78b2de50c501a079ff4b8e41da650982fc
Ricardo Severo <ricardo.severo@quantosobra.com.br>
2024-04-22 13:23:33 -0300

    hyperland fixes

--- .config/hypr/monitor.sh
+++ .config/hypr/monitor.sh
@@ -2,7 +2,12 @@
 
 wlr-randr --output HDMI-A-1  &> /dev/null
 if [[ $? == 0 ]]; then
-    wlr-randr --output eDP-1 --off
+    #wlr-randr --output eDP-1 --off
     #wlr-randr --output HDMI-A-1 --mode 2560x1440 --scale 1
+    if [[ $1 == 'f' ]]; then
+        wlr-randr --output HDMI-A-1 --mode 1920x1080@60 --scale 1
+    else
+        wlr-randr --output HDMI-A-1 --preferred --scale 1
+    fi
     #wlr-randr --output eDP-1 --on --pos 3926,0
 fi
